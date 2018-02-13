1bac2d6d008f60d52b2337e5f1edb1852d8edf7e
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-02-13 13:17:07 +0800

    Remove imagemin as we dont have images to process

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var concat          = require('gulp-concat');
 var uglify          = require('gulp-uglify');
 var sass            = require('gulp-sass');
 var sourceMaps      = require('gulp-sourcemaps');
-var imagemin        = require('gulp-imagemin');
 var minifyCSS       = require('gulp-csso');
 var browserSync     = require('browser-sync');
 var autoprefixer    = require('gulp-autoprefixer');
